402d3e08a753bfdfe2cabbb24452cbe8294ed62d
De'Jour <dejourford@yahoo.com>
2026-04-12 23:57:14 -0500

    update content

---
+++ src/Content.jsx
@@ -0,0 +1,14 @@
+export function Content() {
+    return (
+        <section>
+            <h1>Hedy Lamarr's Todos</h1>
+            <img src="https://react.dev/images/docs/scientists/yXOvdOSs.jpg" alt="Hedy Lamarr" className="photo" />
+
+            <ul>
+                <li>Invent new traffic lights</li>
+                <li>Rehearse a movie scene</li>
+                <li>Improve the spectrum technology</li>
+            </ul>
+        </section>
+    )
+}
