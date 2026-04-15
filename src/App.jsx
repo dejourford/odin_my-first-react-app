50638a2b7c5429cf540c363f35316ae98f996c5d
De'Jour <dejourford@yahoo.com>
2026-04-15 09:31:07 -0500

    increase integer when button clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import { Button } from "./Button"
 function App() {
    const colors = ["pink", "red", "blue", "green", "orange"];
   const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+  const [number, setNumber] = useState(0);
 
   const handleClick = (color) => {
     setBackgroundColor(color);
+    setNumber(number + 1)
   }
 
+  
+
   return (
-    <div style={{backgroundColor}}>
+    <section style={{backgroundColor}}>
       {colors.map((color, index) => 
         <button key={index} onClick={() => handleClick(color)}>{color}</button>
       )}
       
-      
-    </div>
+      <div style={{border: "1px solid black"}} className="tracker">{number}</div>
+
+    </section>
   )
 }
 
